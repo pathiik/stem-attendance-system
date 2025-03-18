19087a0f51e714fca2a1fac4406bcdeb262817d0
Pathik Bhattarai <pathik.b45@gmail.com>
2025-03-18 12:24:21 -0400

    Scanner App Setup & Styles Integration

---
+++ frontend/scanner-app/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Stack } from "expo-router";
+import "./globals.css";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{ title: "Attendance App (Scanner)", headerShown: false }}
+        />
+      </Stack>
+    </SafeAreaProvider>
+  );
+}
