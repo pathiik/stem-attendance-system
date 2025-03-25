5eac6c38d336b49dc70eacb9a0f18cbf32d12dc5
Pathik Bhattarai <pathik.b45@gmail.com>
2025-03-25 14:00:43 -0400

    Scrollable View in Authentication Screen

--- frontend/parent-app/app/screens/AuthScreen.tsx
+++ frontend/parent-app/app/screens/AuthScreen.tsx
@@ -6,6 +6,9 @@ import {
   TouchableOpacity,
   Image,
   StatusBar,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from "react-native";
 import {
   signInWithEmailAndPassword,
@@ -79,114 +82,129 @@ export default function AuthScreen() {
   };
 
   return (
-    <View
-      className="flex-1 justify-start bg-white p-4"
-      style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      className="flex-1 bg-white"
     >
-      {/* Styling the status bar */}
-      <StatusBar
-        barStyle="dark-content"
-        backgroundColor="transparent"
-        translucent
-      />
-
-      {/* STEM Logo Container */}
-      <View className="items-center pt-24 mb-20 bg-white">
-        <Image
-          source={require("../../assets/stem-logo-dark.png")}
-          className="h-20"
-          resizeMode="contain"
-        />
-        <Text className="text-2xl font-bold text-primary">Attendance App</Text>
-        <Text className="text-sm text-gray-500">For Parents</Text>
-      </View>
-
-      <AuthTabs onTabChange={(tab) => setActiveTab(tab)} />
-
-      {error && <Text className="text-red-500 mb-4 text-center">{error}</Text>}
-
-      {/* Form Input */}
-      <View className="w-full mb-4">
-        {/* Email Input Field */}
-        <View className="w-full bg-gray-100 p-3 rounded-lg mb-4">
-          <TextInput
-            className="text-base text-text"
-            placeholder="Email"
-            placeholderTextColor="#4c516d"
-            value={email}
-            onChangeText={setEmail}
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-        </View>
-
-        {/* Password Input */}
-        <View className="w-full bg-gray-100 p-3 rounded-lg mb-4 flex-row items-center">
-          <TextInput
-            className="flex-1 text-base text-text"
-            placeholder="Password"
-            placeholderTextColor="#4c516d"
-            value={password}
-            onChangeText={setPassword}
-            secureTextEntry={!showPassword}
-          />
-          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
-            <MaterialIcons
-              name={showPassword ? "visibility-off" : "visibility"}
-              size={24}
-              color="#4c516d"
-            />
-          </TouchableOpacity>
-        </View>
-
-        {/* Confirm Password Field (for signup only) */}
-        {activeTab === "signup" && (
-          <View className="w-full bg-gray-100 p-3 rounded-lg mb-4 flex-row items-center">
-            <TextInput
-              className="flex-1 text-base text-text"
-              placeholder="Confirm Password"
-              placeholderTextColor="#4c516d"
-              value={confirmPassword}
-              onChangeText={setConfirmPassword}
-              secureTextEntry={!showConfirmPassword}
-            />
-            <TouchableOpacity
-              onPress={() => setShowConfirmPassword(!showConfirmPassword)}
-            >
-              <MaterialIcons
-                name={showConfirmPassword ? "visibility-off" : "visibility"}
-                size={24}
-                color="#4c516d"
-              />
-            </TouchableOpacity>
-          </View>
-        )}
-      </View>
-
-      {/* Login/Signup Button */}
-      <TouchableOpacity
-        className="w-full bg-primary p-4 rounded-lg items-center"
-        onPress={handleAuth}
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
       >
-        <Text className="text-white text-lg font-semibold">
-          {activeTab === "login" ? "Login" : "Signup"}
-        </Text>
-      </TouchableOpacity>
-
-      {/* Note for parents (visible only in signup tab) */}
-      {activeTab === "signup" && (
-        <View className="flex-row bg-blue-50 rounded-lg p-3 mt-4 items-center gap-2">
-          <MaterialIcons
-            name="info"
-            size={20}
-            color="#3b82f6"
-            style={{ opacity: 0.6}}
+        <View
+          className="flex-1 justify-start bg-white p-4"
+          style={{ paddingTop: insets.top, paddingBottom: insets.bottom }}
+        >
+          {/* Styling the status bar */}
+          <StatusBar
+            barStyle="dark-content"
+            backgroundColor="transparent"
+            translucent
           />
-          <Text className="text-sm text-gray-500" style={{ opacity: 0.6 }}>
-            Please use the same email address you used to register your child.
-          </Text>
+
+          {/* STEM Logo Container */}
+          <View className="items-center pt-24 mb-20 bg-white">
+            <Image
+              source={require("../../assets/stem-logo-dark.png")}
+              className="h-20"
+              resizeMode="contain"
+            />
+            <Text className="text-2xl font-bold text-primary">
+              Attendance App
+            </Text>
+            <Text className="text-sm text-gray-500">For Parents</Text>
+          </View>
+
+          <AuthTabs onTabChange={(tab) => setActiveTab(tab)} />
+
+          {error && (
+            <Text className="text-red-500 mb-4 text-center">{error}</Text>
+          )}
+
+          {/* Form Input */}
+          <View className="w-full mb-4">
+            {/* Email Input Field */}
+            <View className="w-full bg-gray-100 p-3 rounded-lg mb-4">
+              <TextInput
+                className="text-base text-text"
+                placeholder="Email"
+                placeholderTextColor="#4c516d"
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+                autoCapitalize="none"
+              />
+            </View>
+
+            {/* Password Input */}
+            <View className="w-full bg-gray-100 p-3 rounded-lg mb-4 flex-row items-center">
+              <TextInput
+                className="flex-1 text-base text-text"
+                placeholder="Password"
+                placeholderTextColor="#4c516d"
+                value={password}
+                onChangeText={setPassword}
+                secureTextEntry={!showPassword}
+              />
+              <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                <MaterialIcons
+                  name={showPassword ? "visibility-off" : "visibility"}
+                  size={24}
+                  color="#4c516d"
+                />
+              </TouchableOpacity>
+            </View>
+
+            {/* Confirm Password Field (for signup only) */}
+            {activeTab === "signup" && (
+              <View className="w-full bg-gray-100 p-3 rounded-lg mb-4 flex-row items-center">
+                <TextInput
+                  className="flex-1 text-base text-text"
+                  placeholder="Confirm Password"
+                  placeholderTextColor="#4c516d"
+                  value={confirmPassword}
+                  onChangeText={setConfirmPassword}
+                  secureTextEntry={!showConfirmPassword}
+                />
+                <TouchableOpacity
+                  onPress={() => setShowConfirmPassword(!showConfirmPassword)}
+                >
+                  <MaterialIcons
+                    name={showConfirmPassword ? "visibility-off" : "visibility"}
+                    size={24}
+                    color="#4c516d"
+                  />
+                </TouchableOpacity>
+              </View>
+            )}
+          </View>
+
+          {/* Login/Signup Button */}
+          <TouchableOpacity
+            className="w-full bg-primary p-4 rounded-lg items-center"
+            onPress={handleAuth}
+          >
+            <Text className="text-white text-lg font-semibold">
+              {activeTab === "login" ? "Login" : "Signup"}
+            </Text>
+          </TouchableOpacity>
+
+          {/* Note for parents (visible only in signup tab) */}
+          {activeTab === "signup" && (
+            <View className="flex-row bg-blue-50 rounded-lg p-3 mt-4 items-center gap-2">
+              <MaterialIcons
+                name="info"
+                size={20}
+                color="#3b82f6"
+                style={{ opacity: 0.6 }}
+              />
+              <Text className="text-sm text-gray-500" style={{ opacity: 0.6 }}>
+                Please use the same email address you used to register your
+                child.
+              </Text>
+            </View>
+          )}
         </View>
-      )}
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
